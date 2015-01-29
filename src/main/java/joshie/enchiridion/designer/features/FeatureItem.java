097e28e29b26d1fdb03243a72329fa762d293567
joshiejack <joshjackwildman@gmail.com>
2015-01-29 23:48:09 +0000

    Abstract out the text editing.

--- src/main/java/joshie/enchiridion/designer/features/FeatureItem.java
+++ src/main/java/joshie/enchiridion/designer/features/FeatureItem.java
@@ -6,18 +6,19 @@ import net.minecraft.item.ItemStack;
 
 import com.google.gson.annotations.Expose;
 
-public class FeatureItem extends Feature {
+public class FeatureItem extends FeatureWithText {
     @Expose
     public String item;
     public ItemStack stack;
     public float size;
-    
+    private String search = "";
+
     public FeatureItem() {
         width = 32;
         height = 32;
         item = "minecraft:stone";
     }
-    
+
     @Override
     public void recalculate(int x, int y) {
         super.recalculate(x, y);
@@ -26,17 +27,29 @@ public class FeatureItem extends Feature {
     }
 
     @Override
+    public String getTextField() {
+        return this.search;
+    }
+
+    @Override
+    public void setTextField(String str) {
+        this.search = str;
+    }
+
+    @Override
     public void drawFeature() {
-        if(stack == null) {
+        if (stack == null) {
             stack = StackHelper.getStackFromString(item);
         }
-        
+
         DesignerHelper.drawStack(stack, left, top, size);
     }
 
     @Override
     public void loadEditor() {
         // DRAW SELECTION OF ITEM
+        //TODO: Display the Search Box with Text
         
+        //TODO: Display the Scrollable list of Items
     }
 }
---
+++ src/main/java/joshie/enchiridion/designer/features/FeatureWithText.java
@@ -0,0 +1,102 @@
+package joshie.enchiridion.designer.features;
+
+import joshie.enchiridion.designer.DesignerHelper;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ChatAllowedCharacters;
+
+import com.google.gson.annotations.Expose;
+
+public abstract class FeatureWithText extends Feature {
+    private int position;
+    private int tick;
+    private boolean white;
+    
+    @Override
+    public void setSelected() {
+        super.setSelected();
+        position = getTextField().length();
+    }
+    
+    public abstract String getTextField();
+    public abstract void setTextField(String str);
+    
+    public String getText() {
+        if (isSelected) {
+            tick++;
+            if (tick % 60 == 0) {
+                if (white) {
+                    white = false;
+                } else {
+                    white = true;
+                }
+            }
+
+            if (white) {
+                return new StringBuilder(getTextField()).insert(Math.min(position, getTextField().length()), "[*cursor*]").toString();
+            } else {
+                return new StringBuilder(getTextField()).insert(Math.min(position, getTextField().length()), "[*/cursor*]").toString();
+            }
+        } else return getTextField();
+    }
+
+    private void cursorLeft(int count) {
+        int left = position - count;
+        if (left < 0) {
+            position = 0;
+        } else position = left;
+    }
+
+    private void cursorRight(int count) {
+        String text = getTextField();
+        int right = position + count;
+        if (right > text.length()) {
+            position = text.length();
+        } else position = right;
+    }
+
+    private void add(String string) {
+        String text = getTextField();
+        StringBuilder builder = new StringBuilder(text);
+        text = builder.insert(position, string).toString();
+        setTextField(text);
+        cursorRight(string.length());
+    }
+
+    private void delete(int count) {
+        String text = getTextField();
+        if ((count < 0 && position > 0) || (count >= 0 && position < text.length())) {
+            StringBuilder builder = new StringBuilder(text);
+            text = builder.deleteCharAt(position + count).toString();
+            setTextField(text);
+            if (count < 0) cursorLeft(-count);
+            else if (count >= 0) cursorRight(count);
+        }
+    }
+
+    @Override
+    public void keyTyped(char character, int key) {
+        if (getTextField() != null && isSelected) {
+            if (key == 203) {
+                cursorLeft(1);
+            } else if (key == 205) {
+                cursorRight(1);
+            } else if (character == 22) {
+                add(GuiScreen.getClipboardString());
+            } else if (key == 14) {
+                delete(-1);
+            } else if (key == 211) {
+                delete(0);
+            } else if (key == 28 || key == 156) {
+                add("\n");
+            } else if (ChatAllowedCharacters.isAllowedCharacter(character)) {
+                add(Character.toString(character));
+            }
+        }
+    }
+
+    @Override
+    public void loadEditor() {
+        // DRAW TEXT+++, TEXT---, BBCODE MODE
+
+    }
+}
