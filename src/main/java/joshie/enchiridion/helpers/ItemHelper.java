35e49d654b009cdd9cfad2efcde630e6d4ad64b2
joshiejack <joshjackwildman@gmail.com>
2015-01-31 19:52:01 +0000

    Don't crash when an item fails to load it's sub items. Fixes #19

--- src/main/java/joshie/enchiridion/helpers/ItemHelper.java
+++ src/main/java/joshie/enchiridion/helpers/ItemHelper.java
@@ -3,13 +3,16 @@ package joshie.enchiridion.helpers;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import joshie.enchiridion.ELogger;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import org.apache.logging.log4j.Level;
+
 public class ItemHelper {
     public static final ArrayList<ItemStack> items = new ArrayList();
-    
+
     public static void init() {
         Iterator iterator = Item.itemRegistry.iterator();
         while (iterator.hasNext()) {
@@ -19,16 +22,22 @@ public class ItemHelper {
                 continue;
             }
 
-            for (CreativeTabs tab : item.getCreativeTabs()) {
-                item.getSubItems(item, tab, items);
+            if (item.getCreativeTabs() != null && item.getCreativeTabs().length > 0) {
+                for (CreativeTabs tab : item.getCreativeTabs()) {
+                    try {
+                        item.getSubItems(item, tab, items);
+                    } catch (Exception e) {
+                        ELogger.log(Level.ERROR, "Enchiridion had an issue when trying to load the item: " + item.getClass());
+                    }
+                }
             }
         }
     }
-    
+
     public static void addInventory() {
         for (ItemStack stack : ClientHelper.getPlayer().inventory.mainInventory) {
             if (stack != null) {
-                if(!items.contains(stack)) {
+                if (!items.contains(stack)) {
                     items.add(stack);
                 }
             }
