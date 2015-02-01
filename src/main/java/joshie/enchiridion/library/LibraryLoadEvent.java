d46ca0975ffe07af9a600caf85797547cc3f5319
joshiejack <joshjackwildman@gmail.com>
2015-02-01 00:14:35 +0000

    Fix books not loading when switching between saves.

--- src/main/java/joshie/enchiridion/library/LibraryLoadEvent.java
+++ src/main/java/joshie/enchiridion/library/LibraryLoadEvent.java
@@ -7,20 +7,15 @@ import cpw.mods.fml.relauncher.Side;
 
 public class LibraryLoadEvent {
     public static LibraryDataServer data = null;
-    public static boolean isInit;
 
     @SubscribeEvent
     public void onLoad(WorldEvent.Load event) {
         if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER) {
-            if (!isInit) {
-                LibraryDataServer.preloadBooks();
-                data = (LibraryDataServer) event.world.loadItemData(LibraryDataServer.class, LibraryDataServer.DATA_NAME);
-                if (data == null) {
-                    data = new LibraryDataServer(LibraryDataServer.DATA_NAME);
-                    event.world.setItemData(LibraryDataServer.DATA_NAME, data);
-                }
-
-                isInit = true;
+            LibraryDataServer.preloadBooks();
+            data = (LibraryDataServer) event.world.loadItemData(LibraryDataServer.class, LibraryDataServer.DATA_NAME);
+            if (data == null) {
+                data = new LibraryDataServer(LibraryDataServer.DATA_NAME);
+                event.world.setItemData(LibraryDataServer.DATA_NAME, data);
             }
         }
     }
