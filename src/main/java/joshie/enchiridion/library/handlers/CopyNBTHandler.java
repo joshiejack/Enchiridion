d702ae136efd678624e561a815cee6d8ee0ad149
joshiejack <joshjackwildman@gmail.com>
2016-09-22 03:09:47 +0100

    Fix copy nbt handler when the library doesn't have a compound tag yet, closes #88

--- src/main/java/joshie/enchiridion/library/handlers/CopyNBTHandler.java
+++ src/main/java/joshie/enchiridion/library/handlers/CopyNBTHandler.java
@@ -3,6 +3,7 @@ package joshie.enchiridion.library.handlers;
 import joshie.enchiridion.library.LibraryHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
 
 public class CopyNBTHandler extends TemporarySwitchHandler {
@@ -17,7 +18,7 @@ public class CopyNBTHandler extends TemporarySwitchHandler {
         if (library.hasTagCompound()) { //Copy the current configs for the library item to the item itself, to be saved
             stack.setTagCompound(library.getTagCompound());
             LibraryHelper.markDirty();
-        }
+        } else library.setTagCompound(new NBTTagCompound());
 
         super.handle(stack, player, hand, slotID, isShiftPressed);
     }
