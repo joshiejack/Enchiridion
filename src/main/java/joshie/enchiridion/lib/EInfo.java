bdd4864aad31dcf0c7159458402780cb9c19245a
GirafiStudios <girafi0@gmail.com>
2017-09-20 22:22:41 +0200

    Made Enchiridion load after Guide API. Fixes #102

--- src/main/java/joshie/enchiridion/lib/EInfo.java
+++ src/main/java/joshie/enchiridion/lib/EInfo.java
@@ -9,6 +9,7 @@ public class EInfo {
     public static final String INITIALS = "E";
     public static final String VERSION = "@VERSION@";
     public static final String ACCEPTED_MC_VERSIONS = "[1.12,1.13)";
+    public static final String DEPENDENCIES = "after:guideapi";
     public static final String GUI_FACTORY_CLASS = "joshie.enchiridion.gui.config.GuiFactory";
     public static final String TEXPATH = "enchiridion:textures/books/";
 
